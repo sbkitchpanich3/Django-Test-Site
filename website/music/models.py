4f82048631ac39f7eda98556dfa77d1b52f873d4
sbkitchpanich3 <bankster007@gmail.com>
2018-09-19 11:00:08 -0800

    Added models

--- website/music/models.py
+++ website/music/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Album(models.Model):
+	artist = models.CharField(max_length=250)
+	title = models.CharField(max_length=500)
+	genre = models.CharField(max_length=100)
+	logo = models.CharField(max_length=1000)
+
+class Song(models.Model):
+	album = models.ForeignKey(Album, on_delete = models.CASCADE)
+	file_type = models.CharField(max_length = 10)
