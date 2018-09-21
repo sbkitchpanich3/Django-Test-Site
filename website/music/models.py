75ec6f19c99e941e53639ba1c7a8cec5325b0fc6
sbkitchpanich3 <bankster007@gmail.com>
2018-09-20 16:27:12 -0800

    Working models
    added classes that work, database is working

--- website/music/models.py
+++ website/music/models.py
@@ -8,6 +8,10 @@ class Album(models.Model):
 	genre = models.CharField(max_length=100)
 	logo = models.CharField(max_length=1000)
 
+	def __str__(self):
+		return self.title + ' - ' + self.artist
+
 class Song(models.Model):
 	album = models.ForeignKey(Album, on_delete = models.CASCADE)
 	file_type = models.CharField(max_length = 10)
+	song_title = models.CharField(max_length = 250)
