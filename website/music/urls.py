213893d956571f60b17440d419aebc88082da84e
sbkitchpanich3 <bankster007@gmail.com>
2018-09-22 09:21:11 -0800

    Shit I dont' remember where i was

--- website/music/urls.py
+++ website/music/urls.py
@@ -17,5 +17,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:album_id', views.detail, name='detail'),
 ]
--- website/music/views.py
+++ website/music/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("<h1>FUCKWAD</h1>")
+
+def detail(request, id):
+	return HttpResponse("<h2>Details for Album ID: " + str(id) + "</h2>")
