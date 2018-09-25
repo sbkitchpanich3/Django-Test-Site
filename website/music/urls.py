61a2e1d5cec88592bbf019c3d78b2cd35dda7237
Sikharin Kitchpanich <bankster007@gmail.com>
2018-09-25 02:52:22 -0800

    Created a template
    Just finished bucky's tutorial 14

--- website/music/urls.py
+++ website/music/urls.py
@@ -18,5 +18,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:album_id', views.detail, name='detail'),
+    path('<int:id>/', views.detail, name='detail'),
 ]
--- website/music/views.py
+++ website/music/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Album
 
 def index(request):
-	return HttpResponse("<h1>FUCKWAD</h1>")
+	all_albums = Album.objects.all()
+	template = loader.get_template('music/index.html')
+	context = {
+		'all_albums': all_albums,
+	}
+	return HttpResponse(template.render(context, request))
 
 def detail(request, id):
 	return HttpResponse("<h2>Details for Album ID: " + str(id) + "</h2>")
