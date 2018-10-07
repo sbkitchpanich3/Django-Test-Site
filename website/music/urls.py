fef12f72c2c67c7a007b1d756fe5a76ddffcddb7
sbkitchpanich3 <bankster007@gmail.com>
2018-10-07 13:08:15 -0800

    Adding bootstrap and static files
    Finished bucky tut 25

--- website/music/urls.py
+++ website/music/urls.py
@@ -19,6 +19,10 @@ from . import views
 app_name = 'music'
 
 urlpatterns = [
+	#/music/
     path('', views.index, name='index'),
+    #/music/<id>
     path('<int:id>/', views.detail, name='detail'),
+    #/music/<id>/favorite/
+    path('<int:id>/favorite/', views.favorite, name='favorite'),
 ]
--- website/music/views.py
+++ website/music/views.py
@@ -1,6 +1,8 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
-from .models import Album
+from .models import Album, Song
+
+app_name = 'music'
 
 def index(request):
 	all_albums = Album.objects.all()
@@ -9,3 +11,20 @@ def index(request):
 def detail(request, id):
 	album = get_object_or_404(Album, pk=id)
 	return render(request, 'music/detail.html', {'album': album})
+
+def favorite(request, id):
+	album = get_object_or_404(Album, pk=id)
+	try:
+		selected_song = album.song_set.get(pk=request.POST['song'])
+	except(KeyError, Song.DoesNotExist):
+		return render(request, 'music/detail.html', {
+			'album': album,
+			'error_message': "You did not select a valid song.",
+			})
+	else:
+		if selected_song.is_favorite:
+			selected_song.is_favorite = False
+		else:
+			selected_song.is_favorite = True
+		selected_song.save()
+		return render(request, 'music/detail.html', {'album': album})
